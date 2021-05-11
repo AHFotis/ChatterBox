847f94e22a2191c8b595b585698ee632451943f5
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-05-10 19:42:44 -0500

    Took off muted for audio teio testing

--- src/Participant.js
+++ src/Participant.js
@@ -66,7 +66,9 @@ const Participant = ({ participant }) => {
     <div className="participant">
       <h3>{participant.identity}</h3>
       <video ref={videoRef} autoPlay={true} />
-      <audio ref={audioRef} autoPlay={true} muted={true} />
+      <audio ref={audioRef} autoPlay={true} 
+      // muted={true} 
+      />
     </div>
   );
 };
